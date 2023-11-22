199818396ba66444624a4d6b2c4cc9234cf14112
Henrik000 <144285383+Henrik000@users.noreply.github.com>
2023-11-22 19:35:18 +0100

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -1,1 +1,9 @@
-
+function rect(width, height, mode, color, x, y) {
+                console.log("function for å tegne et rektangel som pyret, med plassering inkludert");
+                const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+                rect.setAttribute("y", y)
+                rect.setAttribute("x", x)
+                rect.setAttribute("width", width);
+                rect.setAttribute("height", height);
+                rect.setAttribute("fill", color);
+                return rect;
